8629a657e7e7ba5420b753079528b9e2dc1918bd
Muhammad Nasr Ahmad <nasrahmadcheema@hotmail.com>
2021-03-26 15:08:35 +0000

    Step 10, refactoring the code

--- directory.rb
+++ directory.rb
@@ -1,110 +1,119 @@
+@students = []
+
 def interactive_menu
-  # 1. print the menu and ask the user what to do
-  # 2. read the input and save it into a variable
-  # 3. do what the user has asked
-  # 4. repeat from step 1
-  students = []
   loop do
-    puts "1. input the students"
-    puts "2. Show the students"
-    puts "9. exit"
-    menu_selection = gets.chomp
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. input the students"
+  puts "2. Show the students"
+  puts "9. exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(menu_selection)
     case menu_selection
       when "1"
-        students = input_Students
+        input_Students
       when "2"
-        print_header
-        print(students)
-        print_footer(students)
+        show_students
       when "9"
         exit
       else
         puts "I don't know what you meant, please try again"
     end
-  end
 end
+
 def input_Students
   puts "Please enter the names, hobbies, country of birth and height of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   month = ['january', 'february', 'march', 'april', 'may', 'june', 'july',
   'august', 'september', 'octobar', 'november', 'december']
   # get the details
   puts "name?"
-  name = gets.gsub("\n", "")
+  name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     puts "hobbies?"
-    hobbies = gets.gsub("\n", "")
+    hobbies = gets.chomp
     puts "country of birth?"
-    country_of_birth = gets.gsub("\n", "")
+    country_of_birth = gets.chomp
     puts "height?"
-    height = gets.gsub("\n", "")
+    height = gets.chomp
     puts "cohort?"
-    cohort = gets.gsub("\n", "")
+    cohort = gets.chomp
     while !month.include?(cohort) do
       if cohort.empty?
         cohort = 'november'
         break
       end
       puts "invalid month! please enter month again"
-      cohort = gets.gsub("\n", "")
+      cohort = gets.chomp
     end
   # add the student hash to the array
-    students << {
+    @students << {
       name: name,
       hobbies: hobbies,
       country_of_birth: country_of_birth,
       height: height,
       cohort: cohort.to_sym
     }
-    if students.count == 1 
-      puts "Now we have #{students.count} student"
+    if @students.count == 1 
+      puts "Now we have #{@students.count} student"
     else
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{@students.count} students"
     end
     # get another name from the user
     puts "name?"
-    name = gets.gsub("\n", "")
+    name = gets.chomp
   end
-  #return the array of students
-  students
 end
+
 def print_header
   puts "The students of Villains Academy".center(115, "`")
   puts "-------------".center(115)
 end
-def print(students)
-  if !students.empty?
+
+def print_students_list
+  if !@students.empty?
     counter = 0
-    while counter < students.length do
-      puts "#{students[counter][:name]}, Hobbies:#{students[counter][:hobbies]}, Country of birth:#{students[counter][:country_of_birth]}, Height:#{students[counter][:height]} (#{students[counter][:cohort]} cohort)".center(115)
+    while counter < @students.length do
+      puts "#{@students[counter][:name]}, Hobbies:#{@students[counter][:hobbies]}, Country of birth:#{@students[counter][:country_of_birth]}, Height:#{@students[counter][:height]} (#{@students[counter][:cohort]} cohort)".center(115)
       counter += 1
     end
   else
     puts "there are no students".center(115)
   end
 end
+
+def print_footer
+  if @students.empty?
+  elsif @students.count == 1
+    puts "Overall, we have #{@students.count} great student".center(115, "`")
+  elsif
+    puts "Overall, we have #{@students.count} great students".center(115, "`")
+  end
+end
+
 def print_by_cohort(students)
   return if students.empty?
   cohorts_list = students.map { |student| student[:cohort] }
   puts "there are following cohorts"
   puts cohorts_list.uniq
   puts "Which cohort do you want to print?"
-  cohort_to_print = gets.gsub("\n", "").to_sym
+  cohort_to_print = gets.chomp.to_sym
   students.each do |student|
     if student[:cohort] == cohort_to_print
       puts "#{student[:name]}, Hobbies:#{student[:hobbies]}, Country of birth:#{student[:country_of_birth]}, Height:#{student[:height]}".center(115)
     end
-  end
-end
-def print_footer(names)
-  if names.empty?
-  elsif names.count == 1
-    puts "Overall, we have #{names.count} great student".center(115, "`")
-  elsif
-    puts "Overall, we have #{names.count} great students".center(115, "`")
   end
 end
 
