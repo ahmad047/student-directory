c032898296d31b4e360b230c4ff5b8155ad528c4
Muhammad Nasr Ahmad <nasrahmadcheema@hotmail.com>
2021-03-27 18:45:24 +0000

    Step 13, #3 further refactoring

--- directory.rb
+++ directory.rb
@@ -15,27 +15,21 @@ def print_menu
   puts "9. exit"
 end
 
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-
 def process(menu_selection)
-    case menu_selection
-      when "1"
-        input_Students
-      when "2"
-        show_students
-      when "3"
-        save_students
-      when "4"
-        load_students
-      when "9" # 9 because we'll be adding more items  
-        exit
-      else
-        puts "I don't know what you meant, please try again"
-    end
+  case menu_selection
+    when "1"
+      input_Students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "4"
+      load_students
+    when "9" # 9 because we'll be adding more items  
+      exit
+    else
+      puts "I don't know what you meant, please try again"
+  end
 end
 
 def input_Students
@@ -56,6 +50,7 @@ def input_Students
     @height = STDIN.gets.chomp
     puts "cohort?"
     @cohort = STDIN.gets.chomp
+
     while !month.include?(@cohort) do
       if @cohort.empty?
         @cohort = 'november'
@@ -64,16 +59,42 @@ def input_Students
       puts "invalid month! please enter month again"
       @cohort = STDIN.gets.chomp
     end
+
     input_hash
-    if @students.count == 1 
-      puts "Now we have #{@students.count} student"
-    else
-      puts "Now we have #{@students.count} students"
-    end
-    # get another name from the user
-    puts "name?"
+    puts "Now we have #{@students.count} student" if @students.count == 1
+    puts "Now we have #{@students.count} students" if @students.count > 1
+    puts "name?" #get aother name from the user
     @name = STDIN.gets.chomp
   end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def print_header
+  puts "The students of Villains Academy".center(115, "`")
+  puts "-------------".center(115)
+end
+
+def print_students_list
+  if !@students.empty?
+    counter = 0
+    while counter < @students.length do
+      puts "#{@students[counter][:name]}, Hobbies:#{@students[counter][:hobbies]}, Country of birth:#{@students[counter][:country_of_birth]}, Height:#{@students[counter][:height]} (#{@students[counter][:cohort]} cohort)".center(115)
+      counter += 1
+    end
+  else
+    puts "there are no students".center(115)
+  end
+end
+
+def print_footer
+  return if @students.empty?
+  puts "Overall, we have #{@students.count} great student".center(115, "`") if @students.count == 1
+  puts "Overall, we have #{@students.count} great students".center(115, "`") if @students.count > 1
 end
 
 def save_students
@@ -99,9 +120,7 @@ end
 
 def try_load_students
   filename = ARGV.first# first argument from the command line
-  if filename.nil? # if no filename is given then default to students.csv
-    filename = "students.csv"
-  end
+  filename = "students.csv" if filename.nil? # if no filename is given then default to students.csv
   if File.exists?(filename) # if it exists
     load_students(filename)
      puts "Loaded #{@students.count} from #{filename}"
@@ -115,43 +134,15 @@ def input_hash
   @students << {name: @name, hobbies: @hobbies, country_of_birth: @country_of_birth, height: @height, cohort: @cohort.to_sym}
 end
 
-def print_header
-  puts "The students of Villains Academy".center(115, "`")
-  puts "-------------".center(115)
-end
-
-def print_students_list
-  if !@students.empty?
-    counter = 0
-    while counter < @students.length do
-      puts "#{@students[counter][:name]}, Hobbies:#{@students[counter][:hobbies]}, Country of birth:#{@students[counter][:country_of_birth]}, Height:#{@students[counter][:height]} (#{@students[counter][:cohort]} cohort)".center(115)
-      counter += 1
-    end
-  else
-    puts "there are no students".center(115)
-  end
-end
-
-def print_footer
-  if @students.empty?
-  elsif @students.count == 1
-    puts "Overall, we have #{@students.count} great student".center(115, "`")
-  elsif
-    puts "Overall, we have #{@students.count} great students".center(115, "`")
-  end
-end
-
-def print_by_cohort(students)
-  return if students.empty?
-  cohorts_list = students.map { |student| student[:cohort] }
+def print_by_cohort
+  return if @students.empty?
+  cohorts_list = @students.map { |student| student[:cohort] }
   puts "there are following cohorts"
   puts cohorts_list.uniq
   puts "Which cohort do you want to print?"
   cohort_to_print = STDIN.gets.chomp.to_sym
-  students.each do |student|
-    if student[:cohort] == cohort_to_print
-      puts "#{student[:name]}, Hobbies:#{student[:hobbies]}, Country of birth:#{student[:country_of_birth]}, Height:#{student[:height]}".center(115)
-    end
+  @students.each do |student|
+    puts "#{student[:name]}, Hobbies:#{student[:hobbies]}, Country of birth:#{student[:country_of_birth]}, Height:#{student[:height]}".center(115) if student[:cohort] == cohort_to_print
   end
 end
 
